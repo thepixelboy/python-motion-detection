9ed3b878b52974c0014611f94403123c54db3919
Ruben <34570952+thepixelboy@users.noreply.github.com>
2021-12-09 17:38:58 +0100

    feat: create a delta frame

--- motion_detection.py
+++ motion_detection.py
@@ -6,11 +6,16 @@ video = cv2.VideoCapture(0)
 while True:
     check, frame = video.read()
     grayscale_capture = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    grayscale_capture = cv2.GaussianBlur(grayscale_capture, (21, 21), 0)
 
     if first_frame is None:
         first_frame = grayscale_capture
+        continue
 
-    cv2.imshow("Capturing...", grayscale_capture)
+    delta_frame = cv2.absdiff(first_frame, grayscale_capture)
+
+    cv2.imshow("Grayscale Frame", grayscale_capture)
+    cv2.imshow("Delta Frame", delta_frame)
 
     key = cv2.waitKey(1)
 

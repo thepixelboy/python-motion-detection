c047a320fdc9e4ead53a666782528da7910106cc
Ruben <34570952+thepixelboy@users.noreply.github.com>
2021-12-09 17:49:29 +0100

    feat: create a threshold frame

--- motion_detection.py
+++ motion_detection.py
@@ -5,17 +5,21 @@ video = cv2.VideoCapture(0)
 
 while True:
     check, frame = video.read()
-    grayscale_capture = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    grayscale_capture = cv2.GaussianBlur(grayscale_capture, (21, 21), 0)
+    grayscale_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    grayscale_frame = cv2.GaussianBlur(grayscale_frame, (21, 21), 0)
 
     if first_frame is None:
-        first_frame = grayscale_capture
+        first_frame = grayscale_frame
         continue
 
-    delta_frame = cv2.absdiff(first_frame, grayscale_capture)
+    delta_frame = cv2.absdiff(first_frame, grayscale_frame)
+    delta_frame_threshold = cv2.threshold(
+        delta_frame, 30, 255, cv2.THRESH_BINARY
+    )[1]
 
-    cv2.imshow("Grayscale Frame", grayscale_capture)
+    cv2.imshow("Grayscale Frame", grayscale_frame)
     cv2.imshow("Delta Frame", delta_frame)
+    cv2.imshow("Threshold Frame", delta_frame_threshold)
 
     key = cv2.waitKey(1)
 
